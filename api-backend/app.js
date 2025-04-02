c0e15b5d68459caacfde6a3215a9c930ccd62068
jules <jules.artaud@outlook.com>
2025-04-02 14:40:10 +0200

    add: corsMiddleware

--- api-backend/app.js
+++ api-backend/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
-const cors = require('cors');
 const { sequelize, connectToDatabase } = require('./config/database');
 const productRoutes = require('./routes/productRoutes');
 const authRoutes = require('./routes/authRoutes');
 const cartRoutes = require('./routes/cartRoutes');
+const corsMiddleware = require('./middlewares/corsMiddleware');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(corsMiddleware);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
---
+++ api-backend/middlewares/corsMiddleware.js
@@ -0,0 +1,21 @@
+const cors = require('cors');
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false,
+  optionsSuccessStatus: 200,
+};
+
+const corsMiddleware = cors(corsOptions);
+
+module.exports = corsMiddleware;
